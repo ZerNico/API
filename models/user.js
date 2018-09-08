5cc7a048f0a336fd1cd3897525d8f6e5874d86d4
ZerNico <ZerNico.yt@gmail.com>
2018-09-08 18:15:12 +0200

    Add username and update user schema

--- models/user.js
+++ models/user.js
@@ -10,6 +10,12 @@ const userSchema = new mongoose.Schema({
     minlength: 5,
     maxlength: 50
   },
+  username: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  },
   email: {
     type: String,
     required: true,
@@ -23,7 +29,11 @@ const userSchema = new mongoose.Schema({
     minlength: 5,
     maxlength: 1024
   },
-  isAdmin: Boolean
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
 });
 
 userSchema.methods.generateAuthToken = function() { 
@@ -36,6 +46,7 @@ const User = mongoose.model('User', userSchema);
 function validateUser(user) {
   const schema = {
     name: Joi.string().min(5).max(50).required(),
+    username: Joi.string().min(5).max(50).required(),
     email: Joi.string().min(5).max(255).required().email(),
     password: Joi.string().min(5).max(255).required()
   };
