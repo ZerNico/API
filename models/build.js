9fcd0e14494591226c2abee6f49fb4c695bb2426
ZerNico <ZerNico.yt@gmail.com>
2018-09-20 17:57:35 +0200

    Remove useless validation

--- models/build.js
+++ models/build.js
@@ -38,20 +38,18 @@ const Build = mongoose.model('Build', buildSchema);
 
 function validateCreateBuild(build) {
   const schema = {
-    name: Joi.string().min(5).max(255).required(),
     device: Joi.objectId().required(),
     version: Joi.objectId().required(),
-    date: Joi.date().min(0)
+    date: Joi.date()
   };
   return Joi.validate(build, schema);
 }
 
 function validateUpdateBuild(build) {
   const schema = {
-    name: Joi.string().min(5).max(255),
     device: Joi.objectId(),
     Version: Joi.objectId(),
-    date: Joi.date().min(0),
+    date: Joi.date()
   };
   return Joi.validate(build, schema);
 }
